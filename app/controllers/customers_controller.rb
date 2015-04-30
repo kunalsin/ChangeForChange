e3c998e78b4b950d19c6c8dce9d1efcfa5a579d4
Kunal Sinha <mogh@example.com>
2015-04-30 12:08:17 -0700

    made changes

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -15,6 +15,7 @@ class CustomersController < ApplicationController
 
   def new
     @customer = Customer.new
+    user = @customer.build_user
 
   end
 
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,11 +1,78 @@
 class Ability
   include CanCan::Ability
   
+  
   def initialize(user)
     # set user to new User if not logged in
     user ||= User.new # i.e., a guest user
-    
 
+  # set authorizations for different user roles
+    if user.role? :admin
+      # they get to do it all
+      can :manage, :all
+      
+    elsif user.role? :baker
+      # can see a list of all users
+      can :index, Item
+      
+      # they can read their own profile
+      can :show, User do |u|  
+        u.id == user.id
+      end
+      # they can update their own profile
+      can :update, User do |u|  
+        u.id == user.id
+      end
+
+    elsif user.role? :shipper
+      # can see a list of all users
+      can :index, Order
+      
+      # they can read their own profile
+      can :show, User do |u|  
+        u.id == user.id
+      end
+      # they can update their own profile
+      can :update, User do |u|  
+        u.id == user.id
+      end
+
+    elsif user.role? :customer
+
+      # they can read their own profile
+      can :show, User do |u|  
+        u.id == user.id
+      end
+      # they can update/edit their own profile
+      can :update, User do |u|  
+        u.id == user.id
+      end
+
+      # they can read items in their orders
+      can :read, Order do |this_order|  
+        order_items = user.customer.orders.map{|p| p.id.map(&:id)}
+        order_items.include? this_order.id 
+      end
+
+      #can see a list of all the items
+      can :index, Item
+
+      can :create, Address    
+     
+      # they can update tasks in these projects
+      can :update, Address do |this_address|  
+        my_address = user.customer.address.map{|p| p.id.map(&:id)}
+        my_address.include? this_address.id 
+      end
+
+    else
+      # guests can read items covered (plus home pages)
+      # and create a customer account
+      can :read, Item
+      can :create, Customer
+    end
+  end
+end
 
 
 
